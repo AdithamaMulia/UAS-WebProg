ec0b977250bdc7f956449a3ecb113f10a66855ee
Jheno Syechlo <jhenodahlia@gmail.com>
2023-11-19 17:33:06 +0700

    nambah navbar

--- graciaweb-app/resources/views/kelas.blade.php
+++ graciaweb-app/resources/views/kelas.blade.php
@@ -4,6 +4,7 @@
     <title>Class List</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.tailwindcss.com"></script>
     <style>
         h1 {
             background: linear-gradient(to right, #1f67bf, #40d6ff);
@@ -45,15 +46,23 @@
         .no-underline {
             text-decoration: none;
         }
-
+        .box-main{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            max-width: 100%;
+            margin: auto;
+            margin-top : 20px;
+            height: 100%;
+        }
     </style>
 </head>
+@include('navbar')
 <body style="background-color: #a4a4a4;">
-    <div class="container">
-        <h1>Class List</h1>
+    <div class="box-main">
         <br />
         <div class="card mb-3"  style= "width:100%; max-width: 1400px;">
-            <h2 style="padding-left: 15px;">Class</h2>
+            <h2 style="padding-left: 15px; font-size: 24px">Class</h2>
             <div class="card-body">
                 <div class="row">
                 <?php
@@ -61,7 +70,7 @@
                     echo '
                     <div class="col-lg-4 mb-3">
                         <a href="nilai' . $i . '.php" class="card-link no-underline">
-                            <div class="card" style="height: 100%; max-height: 250px; width: 100%; max-width: 400px;">
+                            <div class="card" style="height: 200px; width: 100%; max-width: 400px;">
                                 <div class="blue-top"></div>
                                 <div class="card-body card-text-zoom" style="overflow: auto;">
                                     <p style="color: black;"><b>Kelas ' . $i . '</b></p>
