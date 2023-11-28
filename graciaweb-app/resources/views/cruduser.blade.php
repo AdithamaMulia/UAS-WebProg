57d0759787b9bfca79dd402af1143b0b459b9d10
indentd3q <vincentzq88@gmail.com>
2023-11-28 21:35:12 +0700

    DESIGN CRUD

--- graciaweb-app/resources/views/cruduser.blade.php
+++ graciaweb-app/resources/views/cruduser.blade.php
@@ -1,31 +1,119 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <title>CRUD User</title>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <script src="https://cdn.tailwindcss.com"></script>   
+    <link rel="stylesheet" 
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css"/>
+    <title>CRUD USER</title>
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+    />
     <style>
-        table {
-            border-collapse: collapse;
-            width: 100%;
+      table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+
+    th, td {
+        border: 1px solid #dddddd;
+        padding: 8px;
+        text-align: left;
+    }
+
+    th {
+        background-color: #f2f2f2;
+    }
+
+    tr:nth-child(even) {
+        background-color: #f9f9f9;
+    }
+
+    tr:hover {
+        background-color: #d9d9d9;
+    }
+
+    td a {
+        text-decoration: none;
+        color: blue;
+        transition: color 0.3s ease-in-out;
+    }
+
+    td a:hover {
+        color: darkblue;
+        font-weight: bold;
+    }
+
+    td:hover {
+        transform: scale(1.05);
+        transition: transform 0.3s ease-in-out;
+    }
+    
+            *{
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+        }
+        body{
+            background-color: whitesmoke;
+            margin: 0;
+            padding: 0;
+        }
+        .header{
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
         }
 
-        table, th, td {
-            border: 1px solid #ddd;
+        #addEmpBtn{
+        outline: none;
+        border: 2px solid white;
+        border-radius: 50px;
+        background-color: yellow;
+        font-size: 15px;
+        font-weight: 500;
+        padding: 0px 5px;
+        }
+        .btn-sm{
+        width: 26px;
+        height: 30px;
+        padding: 2px;
+        cursor: pointer;
+        }
+        @media (max-width:470px){
+        .main-heading{
+            font-size:15px;
+        }
+        #addEmpBtn{
+            font-size: 10px;
+        }
         }
 
-        th, td {
-            padding: 8px;
-            text-align: left;
-        }
+    
     </style>
 </head>
 <body>
-    <h2>Welcome, Admin</h2>
+<h2 class="text-3xl font-bold text-blue-pigeon text-center mt-3">Welcome, Admin!!</h2>
         @if(session('success'))
             <div style="color: green;">{{ session('success') }}</div>
         @endif
-    <a user="nav-link" href="{{ url('/home') }}" style="color: black;"><b>Home Page</b></a>
-    <h3>User Murid</h3>
-    <table>
+    <br/>
+    <a href="{{ url('/home') }}" class="ml-2 rounded px-2 py-1">
+    <i class="fa fa-home text-primary"></i>    
+    Home Page</a>
+    <h3 class="text-2xl ml-2 font-sans"><strong>User Murid</strong></h3>
+
+
+    <br />
+    <table class="table table-striped">
+    <thead class="thead-dark">
         <tr>
             <th>ID</th>
             <th>Username</th>
@@ -41,11 +129,13 @@
             <th>Tgl Dibuat</th>
             <th>Edit</th>
         </tr>
+    </thead>
+    <tbody>
         @foreach($users as $key => $user)
             <tr>
-                <td style="max-width:20px;">{{ $user->userID }}</td>
+                <td>{{ $user->userID }}</td>
                 <td>{{ $user->username }}</td>
-                <td>{{ $user->nama_depan }}{{ $user->nama_belakang }}</td>
+                <td>{{ $user->nama_depan }} {{ $user->nama_belakang }}</td>
                 <td>{{ $user->password }}</td>
                 <td>{{ $user->gender }}</td>
                 <td>{{ $user->tanggal_lahir }}</td>
@@ -55,13 +145,26 @@
                 <td>{{ $user->role }}</td>
                 <td>{{ $user->updated_at }}</td>
                 <td>{{ $user->created_at }}</td>
-                <td><a href="{{ url('/adminedituser/update/' . $user->userID) }}">Edit</a></td>
+                <td>
+                    <a href="{{ url('/adminedituser/update/' . $user->userID) }}">
+                        <i class="fa fa-pencil text-primary"></i> Edit
+                    </a>
+                </td>
+
             </tr>
         @endforeach
-    </table>
-    <a href="{{ url('/adminadduser') }}">Add Data</a>
-    <h3>User Guru</h3>
-    <table>
+    </tbody>
+</table>
+        <a href="{{ url('/adminadduser') }}" class="ml-2 border border-primary rounded px-2 py-1">
+            <i class="fa fa-plus text-primary"></i>   
+            Add Data
+        </a>
+    
+    <br/>
+    <br/>
+    <h3 class="text-2xl ml-2 font-sans"><strong>User Guru</strong></h3>
+    <table class="table table-striped">
+    <thead class="thead-dark">
         <tr>
             <th>ID</th>
             <th>Username</th>
@@ -77,11 +180,13 @@
             <th>Tgl Dibuat</th>
             <th>Edit</th>
         </tr>
+    </thead>
+    <tbody>
         @foreach($users as $key => $user)
             <tr>
-                <td style="max-width:20px;">{{ $user->userID }}</td>
+                <td>{{ $user->userID }}</td>
                 <td>{{ $user->username }}</td>
-                <td>{{ $user->nama_depan }}{{ $user->nama_belakang }}</td>
+                <td>{{ $user->nama_depan }} {{ $user->nama_belakang }}</td>
                 <td>{{ $user->password }}</td>
                 <td>{{ $user->gender }}</td>
                 <td>{{ $user->tanggal_lahir }}</td>
@@ -91,15 +196,27 @@
                 <td>{{ $user->role }}</td>
                 <td>{{ $user->updated_at }}</td>
                 <td>{{ $user->created_at }}</td>
-                <td><a href="{{ url('/adminedituser/update/' . $user->userID) }}">Edit</a></td>
+            <td>
+                <a href="{{ url('/adminedituser/update/' . $user->userID) }}">
+                    <i class="fa fa-pencil text-primary"></i> Edit
+                </a>
+            </td>
+
             </tr>
         @endforeach
-    </table>
-    <a href="{{ url('/adminadduser') }}">Add Data</a>
-    <h3>User Admin</h3>
-    <table>
+    </tbody>
+</table>
+<a href="{{ url('/adminadduser') }}" class="ml-2 border border-primary rounded px-2 py-1">
+            <i class="fa fa-plus text-primary"></i>   
+            Add Data
+        </a>
+    <br/>
+    <br/>
+    <h3 class="text-2xl ml-2 font-sans"><strong>User Admin</strong></h3>
+    <table class="table table-striped">
+    <thead class="thead-dark">
         <tr>
-            <th>ID</th>
+            <th style="max-width:20px;">ID</th>
             <th>Username</th>
             <th>Nama</th>
             <th>Password</th>
@@ -113,11 +230,13 @@
             <th>Tgl Dibuat</th>
             <th>Edit</th>
         </tr>
+    </thead>
+    <tbody>
         @foreach($users as $key => $user)
             <tr>
                 <td style="max-width:20px;">{{ $user->userID }}</td>
                 <td>{{ $user->username }}</td>
-                <td>{{ $user->nama_depan }}{{ $user->nama_belakang }}</td>
+                <td>{{ $user->nama_depan }} {{ $user->nama_belakang }}</td>
                 <td>{{ $user->password }}</td>
                 <td>{{ $user->gender }}</td>
                 <td>{{ $user->tanggal_lahir }}</td>
@@ -127,11 +246,25 @@
                 <td>{{ $user->role }}</td>
                 <td>{{ $user->updated_at }}</td>
                 <td>{{ $user->created_at }}</td>
-                <td><a href="{{ url('/adminedituser/update/' . $user->userID) }}">Edit</a></td>
+            <td>
+                <a href="{{ url('/adminedituser/update/' . $user->userID) }}">
+                    <i class="fa fa-pencil"></i> Edit
+                </a>
+            </td>
+
             </tr>
         @endforeach
-    </table>
-    <a href="{{ url('/adminadduser') }}">Add Data</a>
-    <a href="{{ url('/home') }}">Back</a>
+    </tbody>
+</table>
+<a href="{{ url('/adminadduser') }}" class="ml-2 border border-primary rounded px-2 py-1">
+            <i class="fa fa-plus text-primary"></i>   
+            Add Data
+        </a>
+        <br/>
+        <br/>
+    <a href="{{ url('/home') }}" class="ml-2 border border-primary rounded px-2 py-1">
+    <i class="fa fa-arrow-left text-primary"></i>    
+        Back
+    </a>
 </body>
 </html>
