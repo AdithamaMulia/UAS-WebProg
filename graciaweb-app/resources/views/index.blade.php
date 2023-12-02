50a3be3f2df42d6a6242e00ac8bdfe0191796d9b
indentd3q <vincentzq88@gmail.com>
2023-12-02 22:41:36 +0700

    Hilangin SVG

--- graciaweb-app/resources/views/index.blade.php
+++ graciaweb-app/resources/views/index.blade.php
@@ -53,6 +53,10 @@
       width: 100%;
       max-width: 550px;
     }
+    .br{
+      background: linear-gradient(90deg, #edc7b7 0%, #eee2dc 100%); 
+    }
+
     </style>
   </head>
   <body class="l" style="font-family: 'Source Sans Pro', sans-serif;">
@@ -78,35 +82,15 @@
       </div>
     </div>
   </div>
+<div class="br">
+       <br class="br"> </br>
+       <br class="br"> </br>
+       <br class="br"> </br>
+       <br class="br"> </br>
+       <br class="br"> </br>
+       <br class="br"> </br>
 
-        <svg class="wave-top" viewBox="0 0 1439 147" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <defs>
-    <linearGradient id="waveGradient" x1="0%" y1="0%" x2="0%" y2="100%">
-        <stop offset="0%" style="stop-color: #f7a400" /> 
-        <stop offset="50%" style="stop-color: #f3e0e6" /> 
-        <stop offset="100%" style="stop-color: #fffacd" /> 
-    </linearGradient>
-    </defs>
-        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-            <g transform="translate(-1.000000, -14.000000)" fill-rule="nonzero">
-                <g class="wave" fill="url(#waveGradient)">
-                    <path
-                        d="M1440,84 C1383.555,64.3 1342.555,51.3 1317,45 C1259.5,30.824 1206.707,25.526 1169,22 C1129.711,18.326 1044.426,18.475 980,22 C954.25,23.409 922.25,26.742 884,32 C845.122,37.787 818.455,42.121 804,45 C776.833,50.41 728.136,61.77 713,65 C660.023,76.309 621.544,87.729 584,94 C517.525,105.104 484.525,106.438 429,108 C379.49,106.484 342.823,104.484 319,102 C278.571,97.783 231.737,88.736 205,84 C154.629,75.076 86.296,57.743 0,32 L0,0 L1440,0 L1440,84 Z"
-                    ></path>
-                </g>
-                <g transform="translate(1.000000, 15.000000)" fill="#FFFFFF">
-                    <g transform="translate(719.500000, 131.500000) rotate(180) translate(-719.500000, -131.500000)">
-                        <path d="M0,0 C90.7283404,0.927527913 147.912752,27.187927 291.910178,59.9119003 C387.908462,81.7278826 543.605069,89.334785 759,82.7326078 C469.336065,156.254352 216.336065,153.6679 0,74.9732496" opacity="0.100000001"></path>
-                        <path
-                            d="M100,104.708498 C277.413333,72.2345949 426.147877,52.5246657 546.203633,45.5787101 C666.259389,38.6327546 810.524845,41.7979068 979,55.0741668 C931.069965,56.122511 810.303266,74.8455141 616.699903,111.243176 C423.096539,147.640838 250.863238,145.462612 100,104.708498 Z"
-                            opacity="0.100000001"
-                        ></path>
-                        <path d="M1046,51.6521276 C1130.83045,29.328812 1279.08318,17.607883 1439,40.1656806 L1439,120 C1271.17211,77.9435312 1140.17211,55.1609071 1046,51.6521276 Z" opacity="0.200000003"></path>
-                    </g>
-                </g>
-            </g>
-        </g>
-    </svg>
+  </div>
       </div>
     </div>
 
@@ -253,6 +237,7 @@
     </div>
   </div>
   </section>
+  
 
     <script>
       var scrollpos = window.scrollY;
