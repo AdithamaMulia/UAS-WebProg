55433951b9fa2453af0e0d7babc44a2ef504b2e6
Jheno Syechlo <jhenodahlia@gmail.com>
2023-12-03 22:02:19 +0700

    front end absen

--- graciaweb-app/resources/views/absen.blade.php
+++ graciaweb-app/resources/views/absen.blade.php
@@ -4,7 +4,20 @@
     <title>Absen Siswa</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css"/>
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+    />
     <style>
         h1 {
             background: linear-gradient(to right, #1f67bf, #40d6ff);
@@ -21,14 +34,74 @@
             filter: drop-shadow(5px 5px 10px grey);
         }
         table {
-            border: 1px solid #000;
-            border-collapse: collapse;
-            width: 100%;
+        width: 100%;
+        border-collapse: collapse;
+    }
+    th, td {
+        border: 1px solid #dddddd;
+        padding: 8px;
+        text-align: left;
+    }
+    th {
+        background-color: #f2f2f2;
+    }
+    tr:nth-child(even) {
+        background-color: #f9f9f9;
+    }
+    tr:hover {
+        background-color: #d9d9d9;
+    }
+    td a {
+        text-decoration: none;
+        color: blue;
+        transition: color 0.3s ease-in-out;
+    }
+    td a:hover {
+        color: darkblue;
+        font-weight: bold;
+    }
+    td:hover {
+        transform: scale(1.05);
+        transition: transform 0.3s ease-in-out;
+    }
+
+            *{
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
         }
-        th, td {
-            border: 1px solid #000;
-            padding: 8px;
-            text-align: left;
+        body{
+            background-color: whitesmoke;
+            margin: 0;
+            padding: 0;
+        }
+        .header{
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        }
+        #addEmpBtn{
+        outline: none;
+        border: 2px solid white;
+        border-radius: 50px;
+        background-color: yellow;
+        font-size: 15px;
+        font-weight: 500;
+        padding: 0px 5px;
+        }
+        .btn-sm{
+        width: 26px;
+        height: 30px;
+        padding: 2px;
+        cursor: pointer;
+        }
+        @media (max-width:470px){
+        .main-heading{
+            font-size:15px;
+        }
+        #addEmpBtn{
+            font-size: 10px;
+        }
         }
         .box-main{
             display: flex;
@@ -76,7 +149,7 @@
         </script>
     @endif
 
-        <h2 style="padding-left: 15px; font-size: 30px;">Daftar Absen Siswa</h2>
+    <h2 class="text-3xl font-bold text-blue-pigeon mt-3" style="padding-left: 15px;">Daftar Absen Siswa</h2>
         <br />
         <form id="absenForm" action="/absensubmit" method="post">
             @csrf
@@ -100,8 +173,8 @@
             </div>
             <div class="card-body">
                 <div class="row" style="padding-left: 15px;">
-                    <table>
-                        <thead>
+                <table class="table table-striped">
+                    <thead class="thead-dark" style="background-color:black;">
                             <tr>
                                 <th style="width: 20px;">No</th>
                                 <th style="width: 250px;">Nama</th>
